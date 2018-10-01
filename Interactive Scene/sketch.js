6c2e010958bf9ebde97f4c776d9ab61a625af975
CallanF <43047264+CallanF@users.noreply.github.com>
2018-10-01 13:58:11 -0600

    made IntScn fit to hand in

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -25,6 +25,7 @@ let dx;
 let dy;
 let scroller;
 let timeBetween;
+let penguinTrue;
 
 function preload() {
   imgMan = loadImage("assets/Stk.png");
@@ -34,6 +35,7 @@ function preload() {
   imgGun3 = loadImage("assets/Rifle.png");
   imgGun4 = loadImage("assets/PenguinGun.png");
   imgBang = loadImage("assets/bangBang.png");
+  imgBlargh = loadImage("assets/PenguinGunFiring.png");
 }
 
 function setup() {
@@ -54,6 +56,7 @@ function setup() {
   dx = 5;
   dy = 5;
   scroller = 0;
+  penguinTrue = false;
 }
 
 function draw() {
@@ -61,7 +64,7 @@ function draw() {
   displayMan();
   displayGun();
   switchGun();
-  playSound();
+  // playEffects();
  }
 
 function moveMan() {
@@ -97,7 +100,12 @@ function displayGun() {
     image(imgGun1, (x + 9), (y + 10));
   }
   else if ((gun - 3)% 5 === 0) {
-    image(imgGun4, (x + 9), (y + 10));
+    if (penguinTrue === true) {
+      image(imgGun4, (x + 9), (y + 10));
+    }
+    else {
+      gun += 1;
+    }
   }
   else if ((gun - 4)% 5 === 0) {
     image(imgGun2, (x + 9), (y + 10));
@@ -119,30 +127,26 @@ function switchGun() {
   gun = scroller;
 }
 
-function playSound() {
-  while (mouseIsPressed && (gun % 5 === 0)) {
-    timeBetween = 6;
-    image(imgBang, (x + 9), (y + 10));
-    pause(timeBetween);
-  }
-  while (mouseIsPressed && ((gun - 2)% 5 === 0)) {
-    timeBetween = 8;
-    image(imgBang, (x + 9), (y + 10));
-    pause(timeBetween);
-  }
-  while (mouseIsPressed && ((gun - 4)% 5 === 0)) {
-    timeBetween = 4;
-    image(imgBang, (x + 9), (y + 10));
-    pause(timeBetween);
-  }
-  while (mouseIsPressed && ((gun - 1)% 5 === 0)) {
-    timeBetween = 12;
-    image(imgBang, (x + 9), (y + 10));
-    pause(timeBetween);
-  }
-  while (mouseIsPressed && ((gun - 3)% 5 === 0)) {
-    timeBetween = 2;
-    image(imgBang, (x + 9), (y + 10));
-    pause(timeBetween);
-  }
-}
+// function playEffects() {
+//   while (mouseIsPressed && (gun % 5 === 0)) {
+//     timeBetween = 6;
+//     image(imgBang, (x + 20), (y + 10));
+//   }
+//   while (mouseIsPressed && ((gun - 2)% 5 === 0)) {
+//     timeBetween = 8;
+//     image(imgBang, (x + 20), (y + 10));
+//   }
+//   while (mouseIsPressed && ((gun - 4)% 5 === 0)) {
+//     timeBetween = 4;
+//     image(imgBang, (x + 20), (y + 10));
+//   }
+//   while (mouseIsPressed && ((gun - 1)% 5 === 0)) {
+//     timeBetween = 12;
+//     image(imgBang, (x + 20), (y + 10));
+//     pause(timeBetween);
+//   }
+//   while (mouseIsPressed && ((gun - 3)% 5 === 0)) {
+//     timeBetween = 2;
+//     image(imgBlargh, (x + 9), (y + 10));
+//   }
+// }
