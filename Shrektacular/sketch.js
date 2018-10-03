de21d54986a92480a799a9ab0c0a7c695183a44f
CallanF <43047264+CallanF@users.noreply.github.com>
2018-10-03 13:41:43 -0600

    so it begins

--- Shrektacular/sketch.js
+++ Shrektacular/sketch.js
@@ -6,6 +6,8 @@
 // - describe what you did to take this project "above and beyond"
 
 let state;
+let middleX;
+let middleY;
 let imgShrek;
 let imgDonkey;
 let imgFiona;
@@ -15,6 +17,7 @@ let imgSwamp;
 let imgFarquaadCastle;
 let imgDragonCastle;
 let imgWoods;
+let scalar;
 
 function preload() {
   imgShrek = loadImage("assets/Shrek Is Not Drek.png");
@@ -23,12 +26,32 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  middleX = windowWidth/2;
+  middleY = windowHeight/2;
+  scalar = 0.25;
+  state = 0;
 }
 
 function draw() {
+  locationShrek();
   Shrookles();
 }
 
 function Shrookles() {
-  image(imgShrek, mouseX - 250, mouseY - 200, 482, 426);
+  image(imgShrek, mouseX - 481.5 * scalar, mouseY - 426 * scalar, imgShrek.width * scalar, imgShrek.height * scalar);
+}
+
+function locationShrek() {
+  if (state === 0) {
+    image(imgSwamp, 0, 0, windowWidth, windowHeight);
+  }
+  else if (state === 1) {
+    image(imgSwamp, 0, 0, windowWidth, windowHeight);
+  }
+  else if (state === 2) {
+    image(imgSwamp, 0, 0, windowWidth, windowHeight);
+  }
+  else if (state === 3) {
+    image(imgSwamp, 0, 0, windowWidth, windowHeight);
+  }
 }
