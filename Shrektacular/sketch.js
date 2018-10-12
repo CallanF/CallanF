7fe2298447c69634c61ca703541fb8e7d95a2381
CallanF <43047264+CallanF@users.noreply.github.com>
2018-10-12 13:59:01 -0600

    Worked on Shrek's Adventure
    it's coming along nicely

--- Shrektacular/sketch.js
+++ Shrektacular/sketch.js
@@ -9,20 +9,36 @@ let state;
 let middleX;
 let middleY;
 let imgShrek;
-let imgDonkey;
 let imgFiona;
 let imgFarquaad;
-let imgCitizen;
 let imgSwamp;
 let imgFarquaadCastle;
-let imgDragonCastle;
-let imgWoods;
+let imgThrone;
+let imgText1;
+let imgText2;
+let imgTextBox;
+let imgShrlooking;
+let imgArrUp;
+let imgArrDn;
+let imgArrRt;
+let imgArrLf;
 let scalar;
+let textDone;
 
 function preload() {
   imgShrek = loadImage("assets/Shrek Is Not Drek.png");
+  imgFarquaad = loadImage("assets/Farquaad.png");
   imgSwamp = loadImage("assets/Swumpy.png");
   imgFarquaadCastle = loadImage("assets/Duloc.jpg");
+  imgThrone = loadImage("assets/ThroneRoom.jpg");
+  imgText1 = loadImage("assets/Text1AltWhiteReal.png");
+  imgText2 = loadImage("assets/Text2.png");
+  imgTextBox = loadImage("assets/TextBox.png");
+  imgShrlooking = loadImage("assets/ShrekLookingtotheSide - Copy.jpg");
+  imgArrUp = loadImage("assets/ArrUp.png");
+  imgArrDn = loadImage("assets/ArrDown.png");
+  imgArrRt = loadImage("assets/ArrRight.png");
+  imgArrLf = loadImage("assets/ArrLeft.png");
 }
 
 function setup() {
@@ -31,30 +47,49 @@ function setup() {
   middleY = windowHeight/2;
   scalar = 0.25;
   state = 0;
+  textDone = false;
+  imageMode(CENTER);
 }
 
 function draw() {
   locationShrek();
   Shrookles();
+  shrextDisplay();
   detectStateChange();
+  rectMode(CENTER);
+//  rect(middleX, middleY, 300, 400);
 }
 
 function Shrookles() {
-  image(imgShrek, mouseX - 481.5 * scalar, mouseY - 426 * scalar, imgShrek.width * scalar, imgShrek.height * scalar);
+  image(imgShrek, mouseX, mouseY, imgShrek.width * scalar, imgShrek.height * scalar);
 }
 
 function locationShrek() {
   if (state === 0) {
-    image(imgSwamp, 0, 0, windowWidth, windowHeight);
+    image(imgSwamp, middleX, middleY, windowWidth, windowHeight);
+    image(imgArrLf, middleX - 700, middleY, width/10, height/4);
   }
   else if (state === 1) {
-    image(imgFarquaadCastle, 0, 0, windowWidth, windowHeight);
+    image(imgFarquaadCastle, middleX, middleY, windowWidth, windowHeight);
+    image(imgArrRt, middleX + 700, middleY, width/10, height/4);
   }
   else if (state === 2) {
-    image(imgSwamp, 0, 0, windowWidth, windowHeight);
+    image(imgThrone, middleX, middleY, windowWidth, windowHeight);
+    image(imgFarquaad, middleX, middleY, width * scalar, height * scalar);
   }
   else if (state === 3) {
-    image(imgSwamp, 0, 0, windowWidth, windowHeight);
+    image(imgSwamp, middleX, middleY, windowWidth, windowHeight);
+  }
+}
+
+function shrextDisplay() {
+  if (state === 0 && textDone === false) {
+    image(imgTextBox, middleX, middleY + 250, width - 50);
+    image(imgShrlooking, middleX - 500, middleY + 250, width / 3.5, height / 3.5);
+    image(imgText1, middleX + 200, middleY + 280);
+    if (mouseIsPressed && mouseX > 50 && mouseX < windowWidth - 50) {
+      textDone = true;
+    }
   }
 }
 
@@ -62,11 +97,19 @@ function detectStateChange() {
   if (mouseIsPressed && mouseX <= 50) {
     if (state === 0) {
       state = 1;
+      textDone = false;
     }
   }
   if (mouseIsPressed && mouseX >= windowWidth - 50) {
     if (state === 1) {
       state = 0;
+      textDone = false;
+    }
+  }
+  if (mouseIsPressed && mouseX >= middleX - 150 && mouseX <= middleX + 150 && mouseY >= middleY - 200 && mouseY <= middleY + 200) {
+    if (state === 1) {
+      state = 2;
+      textDone = false;
     }
   }
 }
