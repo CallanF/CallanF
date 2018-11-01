7b4e454dc91dc8b923d4b9f0d6c6d4eeddea8ef8
CallanF <43047264+CallanF@users.noreply.github.com>
2018-11-01 14:16:48 -0600

    fixed Shrektacular, almost done

--- Shrektacular/sketch.js
+++ Shrektacular/sketch.js
@@ -5,6 +5,8 @@
 let state;
 let stateChanged;
 let mouseReset;
+let textState;
+let textChanged;
 
 let middleX;
 let middleY;
@@ -33,9 +35,13 @@ let imgSecret;
 
 let imgText1;
 let imgText2;
+let imgText3;
+let imgText4;
 let imgTextBox;
 
+
 let imgShrlooking;
+let imgFrqFace;
 let imgArrUp;
 let imgArrDn;
 let imgArrRt;
@@ -54,10 +60,13 @@ function preload() {
   imgSecret = loadImage("assets/Art, plural noun.png");
 
   imgText1 = loadImage("assets/Text1AltWhite.png");
-  imgText2 = loadImage("assets/Text2.png");
+  imgText2 = loadImage("assets/Text2Coloured.png");
+  imgText3 = loadImage("assets/Text3Coloured.png");
+  imgText4 = loadImage("assets/Text4White.png");
   imgTextBox = loadImage("assets/TextBox.png");
 
   imgShrlooking = loadImage("assets/ShrekLookingtotheSide - Copy.jpg");
+  imgFrqFace = loadImage("assets/FarquaadFace.png");
   imgArrUp = loadImage("assets/ArrUp.png");
   imgArrDn = loadImage("assets/ArrDown.png");
   imgArrRt = loadImage("assets/ArrRight.png");
@@ -70,6 +79,8 @@ function setup() {
   middleY = windowHeight / 2;
   scalar = 0.25;
   state = 0;
+  textState = 1;
+  textChanged = false;
   stateChanged = false;
   textDone = false;
   arrValXR = windowWidth - windowWidth / 10;
@@ -98,9 +109,6 @@ function draw() {
 function Shrookles() {
   if (state !== 3) {
     image(imgShrek, mouseX, mouseY, imgShrek.width * scalar, imgShrek.height * scalar);
-  }
-  if (mouseX >= 50 && mouseX <= windowWidth - 50 && mouseY > 50 && mouseY < windowHeight - 50) {
-    mouseReset = true;
   }
 }
 
@@ -140,7 +148,23 @@ function shrextDisplay() {
       textDone = true;
     }
   }
+  if (state === 2 && textState === 1) {
+    image(imgTextBox, middleX, middleY + 250, width - 50);
+    image(imgFrqFace, middleX - 500, middleY + 250, width / 3.5, height / 3.5);
+    image(imgText2, middleX + 200, middleY + 280, width / 1.5, height / 3.5);
+  }
+  if (state === 2 && textState === 2) {
+    image(imgTextBox, middleX, middleY + 250, width - 50);
+    image(imgShrlooking, middleX - 500, middleY + 250, width / 3.5, height / 3.5);
+    image(imgText3, middleX + 200, middleY + 280, width / 1.5, height / 3.5);
+  }
+  if (state === 2 && textState === 3) {
+    image(imgTextBox, middleX, middleY + 250, width - 50);
+    image(imgFrqFace, middleX - 500, middleY + 250, width / 3.5, height / 3.5);
+    image(imgText4, middleX + 200, middleY + 280, width / 1.5, height / 3.5);
+  }
 }
+
 
 function detectStateChange() {
   if (mouseIsPressed && mouseX <= 50) {
@@ -196,13 +220,20 @@ function detectStateChange() {
 }
 
 function mouseSort() {
-  if (mouseIsPressed === true && stateChanged === true) {
+  if (mouseIsPressed === true && (stateChanged === true || textChanged === true)) {
     mouseReset = false;
   }
   else if (mouseIsPressed === false) {
     mouseReset = true;
   }
-  if (stateChanged === true && mouseIsPressed === false) {
+  if ((stateChanged === true || textChanged === true) && mouseIsPressed === false) {
     stateChanged = false;
+    textChanged = false;
+  }
+  if (mouseIsPressed === true & state === 2) {
+    if (mouseReset === true) {
+      textState += 1;
+      textChanged = true;
+    }
   }
 }
