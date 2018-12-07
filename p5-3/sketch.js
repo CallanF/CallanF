612e6b4ddb6b8561ba7710f34e8efee28773cf5c
CallanF <43047264+CallanF@users.noreply.github.com>
2018-12-07 14:12:19 -0600

    added final project finally

---
+++ p5-3/sketch.js
@@ -0,0 +1,65 @@
+//Aplaceia
+//Initiated December 7, 2018
+//Callan Fehr
+
+let gameState;
+
+class Player {
+  constructor() {
+
+  }
+}
+class NPC {
+  constructor() {
+
+  }
+}
+class Ground {
+  constructor() {
+
+  }
+}
+class Wall {
+  constructor() {
+
+  }
+}
+class Interactable {
+  constructor() {
+
+  }
+}
+class PartyMember {
+  constructor(h, m, o, d, a, e, l, id) {
+    this.health = h;
+    this.magic = m;
+    this.offence = o;
+    this.defence = d;
+    this.agility = a;
+    this.evade = e;
+    this.luck = l;
+  }
+}
+class Enemy {
+  constructor() {
+
+  }
+}
+class SpecialMove {
+  constructor() {
+
+  }
+}
+class Item {
+  constructor() {
+
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
